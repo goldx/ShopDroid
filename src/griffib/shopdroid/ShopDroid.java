a895a577a48e4c792cc2591721df0c54f286f6d3
ben <ben@Lappy-IV.lan>
2009-11-17 02:24:32 +0000

    ShopDroid

---
+++ src/griffib/shopdroid/ShopDroid.java
@@ -0,0 +1,14 @@
+package griffib.shopdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ShopDroid extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
