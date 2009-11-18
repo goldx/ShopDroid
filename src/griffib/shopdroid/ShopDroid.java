21ee0aa9085125e7d3be37a5a9b471065d3b14bc
unknown <Ben@.(none)>
2009-11-18 16:37:23 +0000

    simple ui

--- src/griffib/shopdroid/ShopDroid.java
+++ src/griffib/shopdroid/ShopDroid.java
@@ -1,14 +1,61 @@
 package griffib.shopdroid;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+//import android.widget.TextView;
 
 public class ShopDroid extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+  
+  static final int DIALOG_MSG_SENT = 0;
+  
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    
+    //Find button
+    Button sendMsg = (Button)findViewById(R.id.message_button);
+    sendMsg.setOnClickListener(msgButtonListener);
+  }
+  
+  protected Dialog onCreateDialog(int id) {
+    AlertDialog msgSentDialog;
+    switch(id) {
+      case DIALOG_MSG_SENT:
+         AlertDialog.Builder buildMsgSent = new AlertDialog.Builder(this);
+         buildMsgSent.setMessage("Message Sent...")
+                     .setNeutralButton("Okay", new DialogInterface.OnClickListener() {
+                      
+                      public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();                        
+                      }
+                    });
+         msgSentDialog = buildMsgSent.create();
+         
+        break;
+      default:
+        msgSentDialog = null;
     }
+    return msgSentDialog;
+  }
+  
+  /**
+   * Listener for sendMsg button
+   */
+  private OnClickListener msgButtonListener = new OnClickListener() {
+  
+    // On a click send a message and tell the user
+    public void onClick(View v) {
+      // Message dialog
+      showDialog(DIALOG_MSG_SENT);
+    }
+  
+  };
 }
